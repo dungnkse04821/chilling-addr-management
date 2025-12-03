0c16849b59595d804e98f935afd87448929feaaa
DungNK <dungnkse04821@gmail.com>
2025-12-03 23:53:16 +0700

    add session

---
+++ UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace ChillingAddrManagement
+{
+    // Lưu trạng thái của cuộc hội thoại
+    public class UserSession
+    {
+        public string Step { get; set; } = "NONE"; // Bước hiện tại (NAME, TYPE, ADDRESS...)
+        public LocationNote DraftData { get; set; } = new LocationNote(); // Dữ liệu đang nhập dở
+    }
+}
